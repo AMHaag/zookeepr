1facc94ba662cecd455c6ba77b73d26e8d33a2cf
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-06-12 23:01:45 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -3,31 +3,47 @@ const { animals } = require("./data/animals.json");
 
 const app = express();
 
-function filterByQuery(query, animalsArray){
-  let filteredResults = animalsArray
-  if(query.diet){
-    filteredResults.filter(animal=>animal.diet===query.diet)
-  }
-  if(query.species){
-    filteredResults = filteredResults.filter(animal=>animalspecies === query.species)
-  }
-  if (query.name){
-    filteredResults = filteredResults.filter(animals=>animals.name===query.name)
-  }
-return filteredResults;
+function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
+  let filteredResults = animalsArray;
 
+  if (query.personalityTraits) {
+    if (typeof query.personalityTraits === "string") {
+      personalityTraitsArray = [query.personalityTraits]
+    }
+    else {personalityTraitsArray = query.personalityTraits}
+  personalityTraitsArray.forEach(trait => {
+    filteredResults = filteredResults.filter(
+      animal=>animal.personalityTraits.indexOf(trait) !== -1
+    )
+    
+  });
+  }
 
+  if (query.diet) {
+    filteredResults.filter((animal) => animal.diet === query.diet);
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(
+      (animal) => animalspecies === query.species
+    );
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(
+      (animals) => animals.name === query.name
+    );
+  }
+  return filteredResults;
 }
 
 app.get("/api/animals", (req, res) => {
-  let results = animals
-  if(req.query){
-    results = filterByQuery(req.query,results)
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
   }
-  res.json(results)
+  res.json(results);
 });
 
 app.listen(3001, () => {
   console.log(`API server now on port 3001`);
 });
-
